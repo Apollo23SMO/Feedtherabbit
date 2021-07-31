3435f493162ca632dc6810dace43e3bf443e34b1
catmario123 <85381172+catmario123@users.noreply.github.com>
2021-07-31 15:03:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,53 @@
-var garden,rabbit;
-var gardenImg,rabbitImg;
+var garden, rabbit;
+var gardenImg, rabbitImg;
 var apple, appleImg;
 
-function preload(){
+function preload() {
   gardenImg = loadImage("garden.png");
   rabbitImg = loadImage("rabbit.png");
-  appleImg= loadImage("apple.png")
+  appleImg = loadImage("apple.png")
 }
 
-function setup(){
-  
-  createCanvas(400,400);
-  
-// Moving background
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
+function setup() {
 
-//creating boy running
-rabbit = createSprite(180,340,30,30);
-rabbit.scale =0.09;
-rabbit.addImage(rabbitImg);
+  createCanvas(400, 400);
+
+  // Moving background
+  garden = createSprite(200, 200);
+  garden.addImage(gardenImg);
+
+  //creating boy running
+  rabbit = createSprite(300, 340, 30, 30);
+  rabbit.scale = 0.09;
+  rabbit.addImage(rabbitImg);
 }
 
 
 function draw() {
   background(0);
-  
-  edges= createEdgeSprites();
+
+  edges = createEdgeSprites();
   rabbit.collide(edges);
 
-  rabbit.x=World.mouseX;
-  rabbit.y=World.mouseY;
+  var select_sprites = Math.round(random(1, 3)); 
+  if (frameCount % 40 == 0) {
+        appleSpawn(); 
+      }
 
-  if (rabbit.isTouching(apple)){
-  apple.destroy();
-  }
+
+  rabbit.x = World.mouseX;
 
   drawSprites();
 }
 
-appleSpawn();
+function appleSpawn() {
 
-function appleSpawn(){
-    
-  apple= createSprite(50,50,50,50);
-   apple.addImage(appleImg)
-  apple.scale=0.5
+  apple = createSprite(50, 50, 50, 50);
+  apple.addImage(appleImg)
+  apple.scale = 0.09
 
-apple.y=Math.round(random(50,200))
-apple.x=Math.round(random(50,200))
+  apple.lifetime = 25;
+
+  apple.y = Math.round(random(50, 150))
+  apple.x = Math.round(random(50, 350))
 }
